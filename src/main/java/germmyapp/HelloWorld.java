eaa263b0734817a1b412745a5d7cb9d1031cec08
wandejun <wandejun@nonobank.com>
2016-03-11 13:39:17 +0800

    second commit

---
+++ src/main/java/germmyapp/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (c) 2009-2015. 上海诺诺镑客 All rights reserved.
+ * @(#) HelloWorld.java 2016年3月11日 下午1:37:02
+ */
+package germmyapp;
+/**
+ * @FileName: HelloWorld.java
+ * @Function:TODO
+ * @date: 2016年3月11日 下午1:37:02
+ * @author: wandejun
+ * @since JDK 1.8
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+	}
+
+}
